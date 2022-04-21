5d54fcef6bee5d41a77e1c78bdeedc9c293d8236
Nxisgod007 <jithman.saini@coalitiontechnologies.com>
2022-04-21 13:23:03 -0700

    added links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,9 +10,9 @@ const Footer = () => {
             <Grid templateColumns={["repeat(1, 1fr)", "repeat(3, 1fr)"]} color="white" fontWeight='bold'>
                 <Image src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Bath_Bomb_-_Product-5_-_trans_1.png?v=1610123549" loading='lazy' alt='' />
                 <VStack p="2rem">
-                    <Link to='/'>The Green Blast</Link>
-                    <Link to="/">The Blue Berry</Link>
-                    <Link to="/">The Yellow Mellow</Link>
+                    <Link to='/products/green-blast'>The Green Blast</Link>
+                    <Link to="/products/blue-berry">The Blue Berry</Link>
+                    <Link to="/products/yellow-mellow">The Yellow Mellow</Link>
                 </VStack>
                 <VStack p="2rem">
                     <Link to="/">About Us</Link>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,7 @@ const NavBar = () => {
     return (
         <Flex flexDir='row' justifyContent='space-between' p='2rem' backgroundColor='#ffa8e2'>
             <Icon fill="white" cursor="pointer" onClick={() => openMenu()} as={MdMenu} w={30} h={30}></Icon>
-            <Link to="/"><Image src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg?v=1610055540" w={100} h={100} loading='lazy' alt='' /></Link>
+            <Link to="/"><Image src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg?v=1610055540" w={100} h={100} loading='lazy' alt='' objectFit='contain' /></Link>
             <Box>
                 <Icon fill="white" cursor="pointer" onClick={() => openCart()} as={MdShoppingBasket} w={30} h={30}></Icon>
                 <Badge backgroundColor="#FF38BD" borderRadius="50%">{checkout?.lineItems?.length}</Badge>
